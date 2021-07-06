43cfc878f354aeb29764fa8ad94a51ad70d40f53
jiroyamada <poirot343@outlook.jp>
2021-07-07 06:03:43 +0900

    stepfunction

---
+++ ch3/step_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 05:51:38 2021
+
+@author: okubohironobu
+"""
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x,y)
+plt.ylim(-0.1, 1.1)
+plt.show()
