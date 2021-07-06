67fb555e86d75073738634c02decd69e43ccca16
jiroyamada <poirot343@outlook.jp>
2021-07-07 05:23:50 +0900

    XOR_GATE

---
+++ ch2/xor_gate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 04:48:14 2021
+
+@author: okubohironobu
+"""
+
+from and_gate import AND
+from or_gate import OR
+from nand_gate import NAND
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+if __name__ == '__main__':
+    for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+        y = XOR(xs[0], xs[1])
+        print(str(xs) + " -> " + str(y))
