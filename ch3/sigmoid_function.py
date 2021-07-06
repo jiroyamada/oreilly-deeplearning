d3d7830e2b606fff1925bd5cefcd22cb701033b3
jiroyamada <poirot343@outlook.jp>
2021-07-07 06:21:31 +0900

    sigmoidfunction

--- ch3/sigmoid_function.py
+++ ch3/sigmoid_function.py
@@ -6,3 +6,15 @@ Created on Wed Jul  7 06:02:09 2021
 @author: okubohironobu
 """
 
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))    
+
+X = np.arange(-5.0, 5.0, 0.1)
+Y = sigmoid(X)
+plt.plot(X, Y)
+plt.ylim(-0.1, 1.1)
+plt.show()
