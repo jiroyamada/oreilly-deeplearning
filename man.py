a3a3f6583fbae35fb22857abb6e4d314a05ae775
大久保宏展 <>
2021-07-07 01:21:14 +0900

    クラス練習

---
+++ man.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 01:09:04 2021
+
+@author: okubohironobu
+"""
+
+class Man:
+    def __init__(self, name):
+        self.name = name
+        print("Initialized!")
+        
+    def hello(self):
+        print("Hello " + self.name + "!")
+        
+    def goodbye(self):
+        print("Good-bye " + self.name + "!")
+    
+
+m = Man("David")
+#m.hello()
+#m.goodbye()
