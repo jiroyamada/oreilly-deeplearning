ff6ce2389a765d75d69e84b5305302b7d8a6019c
jiroyamada <poirot343@outlook.jp>
2021-07-07 05:14:18 +0900

    AND_GATE

---
+++ ch2/and_gate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 02:39:41 2021
+
+@author: okubohironobu
+"""
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1,x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+    
+AND(0, 0)
+AND(1, 0)
+AND(0, 1)
+AND(1, 1)
